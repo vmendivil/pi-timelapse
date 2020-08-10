df515181f5882f3812a23c685c2511bdd95e5542
Victor Mendivil <tolish_@hotmail.com>
2020-08-10 18:44:44 -0400

    Updated timelapse app.

--- timelapse.py
+++ timelapse.py
@@ -59,15 +59,17 @@ def capture_image():
 
         # Set a timer to take another picture at the proper interval after this
         # picture is taken.
-        if (image_number < (config['total_images'] - 1)):
+        if (image_number <= (config['total_images'])):
             thread = threading.Timer(config['interval'], capture_image).start()
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print("{} of {} at {}".format(image_number, config['total_images'], now))
 
         # Start up the camera.
         camera = PiCamera()
         set_camera_options(camera)
 
         # Capture a picture.
-        camera.capture(dir + '/image{0:05d}.jpg'.format(image_number))
+        camera.capture(dir + '/image-{0:05d}.jpg'.format(image_number))
         camera.close()
 
         if (image_number < (config['total_images'] - 1)):
@@ -83,20 +85,21 @@ def capture_image():
 # Create directory based on current timestamp.
 dir = os.path.join(
     sys.path[0],
-    'series-' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    'Timelapse-' + datetime.now().strftime('%Y%m%d_%H%M%S')
 )
 create_timestamped_dir(dir)
 
 # Kick off the capture process.
+print '\nTime-lapse capture started!\n'
 capture_image()
 
 # TODO: These may not get called after the end of the threading process...
 # Create an animated gif (Requires ImageMagick).
 if config['create_gif']:
     print '\nCreating animated gif.\n'
-    os.system('convert -delay 10 -loop 0 ' + dir + '/image*.jpg ' + dir + '-timelapse.gif')  # noqa
+    os.system('convert -delay 10 -loop 0 ' + dir + '/image-%05d.jpg ' + dir + '-timelapse.gif')  # noqa
 
 # Create a video (Requires avconv - which is basically ffmpeg).
 if config['create_video']:
     print '\nCreating video.\n'
-    os.system('avconv -framerate 20 -i ' + dir + '/image%05d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')  # noqa
+    os.system('ffmpeg -framerate 10 -i ' + dir + '/image-%05d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')  # noqa
