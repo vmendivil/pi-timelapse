211460443030527860fe79442c838d123b8648fc
Victor Mendivil <tolish_@hotmail.com>
2020-08-10 22:03:51 -0400

    Refactoring timelapse functions.

--- timelapse.py
+++ timelapse.py
@@ -53,53 +53,58 @@ def set_camera_options(camera):
     return camera
 
 
+def create_timelapse_media():
+    # Create an animated gif (Requires ImageMagick).
+    if config['create_gif']:
+        print '\nCreating animated gif.\n'
+        os.system('convert -delay 10 -loop 0 ' + dir + '/image-*.jpg ' + dir + '/timelapse.gif') 
+
+    # Create a video (Requires ffmpeg).
+    if config['create_video']:
+        print '\nCreating video.\n'
+        os.system('ffmpeg -framerate 10 -i ' + dir + '/image-%05d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')
+
+
 def capture_image():
     try:
         global image_number
 
-        # Set a timer to take another picture at the proper interval after this
-        # picture is taken.
-        if (image_number <= (config['total_images'])):
+        # Set a timer to take another picture at the proper interval after this picture is taken
+        if (image_number < (config['total_images'])):
+            # Sleep and call "new" thread
             thread = threading.Timer(config['interval'], capture_image).start()
+
+            # Update counter
+            image_number += 1
+
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print("{} of {} at {}".format(image_number, config['total_images'], now))
 
-        # Start up the camera.
-        camera = PiCamera()
-        set_camera_options(camera)
+            # Start up the camera.
+            camera = PiCamera()
+            set_camera_options(camera)
 
-        # Capture a picture.
-        camera.capture(dir + '/image-{0:05d}.jpg'.format(image_number))
-        camera.close()
-
-        if (image_number < (config['total_images'] - 1)):
-            image_number += 1
+            # Capture a picture.
+            camera.capture(dir + '/image-{0:05d}.jpg'.format(image_number))
+            camera.close()
         else:
             print '\nTime-lapse capture complete!\n'
             # TODO: This doesn't pop user into the except block below :(.
+            create_timelapse_media()
             sys.exit()
 
     except KeyboardInterrupt, SystemExit:
         print '\nTime-lapse capture cancelled.\n'
+
 
 # Create directory based on current timestamp.
 dir = os.path.join(
     sys.path[0],
     'Timelapse-' + datetime.now().strftime('%Y%m%d_%H%M%S')
 )
-create_timestamped_dir(dir)
 
 # Kick off the capture process.
 print '\nTime-lapse capture started!\n'
+
+create_timestamped_dir(dir)
 capture_image()
-
-# TODO: These may not get called after the end of the threading process...
-# Create an animated gif (Requires ImageMagick).
-if config['create_gif']:
-    print '\nCreating animated gif.\n'
-    os.system('convert -delay 10 -loop 0 ' + dir + '/image-%05d.jpg ' + dir + '-timelapse.gif')  # noqa
-
-# Create a video (Requires avconv - which is basically ffmpeg).
-if config['create_video']:
-    print '\nCreating video.\n'
-    os.system('ffmpeg -framerate 10 -i ' + dir + '/image-%05d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')  # noqa
